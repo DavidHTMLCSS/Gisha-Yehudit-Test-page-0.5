592f0d9d066ebe4a65f0fb43ddb69304905e1731
David-MIckle <138385696+DavidHTMLCSS@users.noreply.github.com>
2024-06-22 20:22:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function toggleMenu() {
         body.style.overflow = 'hidden'; // Заблокировать прокрутку страницы
     }
 }
+
+// Добавляем обработчики событий для пунктов меню
+document.querySelectorAll('#context-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+
+        var targetId = this.getAttribute('href').substring(1); // Получаем ID целевого элемента
+        var targetElement = document.getElementsByName(targetId)[0]; // Находим целевой элемент по имени
+
+        if (targetElement) {
+            // Плавный переход к якорю
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+
+            // Закрываем меню
+            var menu = document.getElementById('context-menu');
+            var overlay = document.getElementById('page-overlay');
+            var body = document.body;
+
+            menu.classList.remove('open');
+            overlay.classList.remove('open');
+            body.style.overflow = 'auto'; // Возобновить прокрутку страницы
+        }
+    });
+});
